daef2bb1dfe2e1caa262c4ea76d0c910f0c25550
Your Name <52110617+kainovaii@users.noreply.github.com>
2026-02-13 17:39:33 +0100

    Dev (Rework ControllerLoader (remove route), add support SSE & WebSocket support)

---
+++ src/main/java/fr/kainovaii/obsidian/core/web/websocket/WebSocketLoader.java
@@ -0,0 +1,37 @@
+package fr.kainovaii.obsidian.core.web.websocket;
+
+import fr.kainovaii.obsidian.core.web.route.methods.WebSocket;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+import static spark.Spark.webSocket;
+
+public class WebSocketLoader
+{
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketLoader.class);
+
+    public static void registerWebSockets()
+    {
+        Reflections reflections = new Reflections("fr.kainovaii.obsidian.app.websockets");
+        Set<Class<?>> webSocketClasses = reflections.getTypesAnnotatedWith(WebSocket.class);
+
+        for (Class<?> wsClass : webSocketClasses) {
+            registerWebSocket(wsClass);
+        }
+
+        logger.info("Loaded {} WebSocket handlers", webSocketClasses.size());
+    }
+
+    private static void registerWebSocket(Class<?> wsClass)
+    {
+        WebSocket annotation = wsClass.getAnnotation(WebSocket.class);
+        String path = annotation.value();
+
+        webSocket(path, wsClass);
+
+        logger.debug("Registered WebSocket: {} -> {}", wsClass.getSimpleName(), path);
+    }
+}
